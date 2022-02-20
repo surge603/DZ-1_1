3394e0a0b3b643a21f1c84f0ecd5e78b46a60ac2
Artem <40538552+surge603@users.noreply.github.com>
2022-02-20 11:04:23 +0300

    Задание 8

---
+++ tsk8-evens_from_1_to_n/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 8: Показать чётные числа от 1 до N.
+int N =13;
+int count =2;
+Console.Clear();
+Console.WriteLine("Четные числа от 1 до "+N+":");
+if (N<count) Console.WriteLine("Отсутствуют");
+
+while (count <= N){
+Console.WriteLine(count);
+count +=2;
+}
