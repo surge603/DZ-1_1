0f5f4ae167fb518b233e14ccddb8a2dc7a87ea79
Artem <40538552+surge603@users.noreply.github.com>
2022-02-20 10:23:08 +0300

    Задания 2 и 4

---
+++ tsk2-min_and_max/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Даны два числа. Показать большее и меньшее число
+int num1 = 7;
+int num2 = 9;
+Console.Clear();
+Console.Write("Даны два числа: ");
+Console.Write(num1);
+Console.Write(" и ");
+Console.WriteLine(num2);    
+if (num1==num2)
+{
+Console.WriteLine("Числа равны");
+}
+else if (num1>num2)
+{
+    Console.WriteLine("Большее из чисел - " + num1);
+    Console.WriteLine("Меньшее из чисел - " + num2);
+}else 
+{
+    Console.WriteLine("Большее из чисел - " + num2);
+    Console.WriteLine("Меньшее из чисел - " + num1);
+}
---
+++ tsk4-max_from_3_nums/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Найти максимальное из трёх чисел.
+
+int num1 = 13;
+int num2 = 25;
+int num3 = 7;
+
+Console.Clear();
+
+Console.Write("Даны три числа: ");
+Console.Write(num1);
+Console.Write(", ");
+Console.Write(num2);  
+Console.Write(" и ");
+Console.WriteLine(num3);  
+Console.Write("Максимальным из этих чисел является ");
+
+if (num1>=num2 && num1 >= num3) Console.WriteLine(num1);  
+else if (num2>=num1 && num2 >= num3) Console.WriteLine(num2);  
+else Console.WriteLine(num3);  
