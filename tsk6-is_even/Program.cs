c6878c2decfee48b5861ee6167c0d54ccc565322
Artem <40538552+surge603@users.noreply.github.com>
2022-02-20 10:43:07 +0300

    Задание 6

---
+++ tsk6-is_even/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 6: Выяснить является ли число чётным
+int a = 235;
+
+Console.Clear();
+Console.Write("Число " + a);
+Console.Write(" является ");  
+
+int b = a/2;
+if (a==b*2) Console.WriteLine ("ЧЕТНЫМ");  
+else Console.WriteLine("нечетным");  
